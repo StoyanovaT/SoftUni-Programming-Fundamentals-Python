51961e538aae143d7a8776d5c1572e0b9664b9c5
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2022-05-29 23:20:20 +0300

    Add files via upload

---
+++ 03.lists_basics/05.tic_tac_toe.py
@@ -0,0 +1,13 @@
+row_1 = list(input().split(" "))
+row_2 = list(input().split(" "))
+row_3 = list(input().split(" "))
+
+
+
+
+# for i in range(1, 4):
+#     winner = False
+#     if line_1[i] == line_1[i+1] == line_1[i+2]:
+#         winner = True
+#     if line_1[i] == line_1[i+1] == line_1[i+2]:
+
