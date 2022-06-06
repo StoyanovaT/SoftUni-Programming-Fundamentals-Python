26df8f96c32a3c4562bd2087fe2f48b5c2572779
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2022-06-06 18:09:16 +0300

    Add files via upload

---
+++ 04.functions/06.calculate_rectangle_area.py
@@ -0,0 +1,16 @@
+def area_calc(width,height):
+    return width * height
+
+
+current_w = int(input())
+current_h = int(input())
+
+print(area_calc(current_w,current_h))
+
+# area = lambda width,height: width * height
+#
+# current_width = int(input())
+# current_height = int(input())
+#
+# print(area(current_width, current_height))
+
