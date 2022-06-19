c4badd4af1ac9ede37e9cf47ca8af1b4627c7175
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2022-06-19 22:51:16 +0300

    Add files via upload

---
+++ 08.dictionaries/01.count_chars_in_a_string.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+word = input()
+result = Counter(word)
+
+for key, value in result.items():
+    if key != " ":
+        print(f"{key} -> {value}")
+
+# или:
+# my_dict = {}
+# word = input()
+#
+# for ch in word:
+#     if ch not in my_dict and ch != " ":
+#         my_dict[ch] = 1
+#     else:
+#         my_dict[ch] += 1
+#
+# print(my_dict)
